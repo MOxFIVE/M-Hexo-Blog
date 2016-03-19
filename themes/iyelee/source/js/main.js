1c2a8c8228a6ba6ff2fee625aa0470638d64f84c
MOxFIVE <mo10005@outlook.com>
2016-03-20 01:38:08 +0800

    feat: i18n,  multi-language support

--- themes/iyelee/source/js/main.js
+++ themes/iyelee/source/js/main.js
@@ -160,7 +160,7 @@ require([], function (){
     // Hide Labels
     if(yiliaConfig.isArchive || yiliaConfig.isTag || yiliaConfig.isCategory) {
         $(document).ready(function() {
-            $("#footer").after("<button class='hide-labels'>TAG 开关</button>");
+            $("#footer").after("<button class='hide-labels'>TAGS</button>");
             $(".hide-labels").click(function() {
                 $(".article-info").toggle(200);
             });

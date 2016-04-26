6b29bb377d781944a4b9364b3893c20b043999a1
MOxFIVE <mo10005@outlook.com>
2016-04-27 03:09:48 +0800

    feat: Task Lists in markdown

--- themes/iyelee/source/js/main.js
+++ themes/iyelee/source/js/main.js
@@ -176,5 +176,36 @@ require([], function (){
         }
         $(".article-entry > pre").css("background-color", highlight.background);
         $(".article-entry > pre code").css("color", highlight.foreground);
+    });
+
+    // Task lists in markdown
+    $('ul > li').each(function() {
+        var taskList = {
+            field: $(this).text().substring(0, 2),
+            check: function(str) {
+                var re = new RegExp(str);
+                return this.field.match(re);
+            }
+        }
+
+        var string = ["[ ]", ["[x]", "checked"]];
+        var checked = taskList.check(string[1][0]);
+        var unchecked = taskList.check(string[0]);
+
+        var $current = $(this);
+        function add(str, check) {
+            $current.html($current.html().replace(str, ""));
+            // In order  html > prepend
+            $current.prepend("<input type='checkbox'" + check + ">");
+        }
+
+        if (checked || unchecked) {
+            $(this).addClass('task-list');
+            if (checked) {
+                add(string[1][0], string[1][1]);
+            } else {
+                add(string[0], "");
+            }
+        }
     })
 });
